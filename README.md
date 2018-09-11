74fa4b8d9811b4647e91a27c494f7d71b60fcbe1
Garrett <gjblythe@gmail.com>
2018-09-10 18:20:00 -0700

    final

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # GifTastic
 
-
+API Key
 5Ctjw2fKwwsi9x1iIjvLF0sQ3SYD2jbY
 
+
+CLick any button to load 10 gifs from Giphy.
+    
+On Click for each image will start and stop animation by changin it's URL.
+
+User can enter a custom button by clicking the Use Choose button after entering Text.
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,92 +1,116 @@
-
-var gifs = ["Patric Stewart", "Cats", "Ewoks", "Bruce Campbell", "Rick James", "Dave Chappelle", "Porkins", "Goonies", "Soundwave", "Mad Max", "Mario", "Skeletor", "Han Shot First"];
+var gifs = [
+  "Patric Stewart",
+  "Cat Breading",
+  "Ewoks",
+  "Bruce Campbell",
+  "Rick James",
+  "Dave Chappelle",
+  "Porkins",
+  "Goonies",
+  "Wrath of Khan",
+  "Mad Max",
+  "Mario",
+  "Skeletor",
+  "Han Shot First",
+  "Pugs",
+  "South Park"
+];
 var newButton = [];
 
 
-// disapears
-$("#userSelected").mousedown(function(){
-    var text = $('#select').val().trim();
-    newButton.push(text);
-    console.log(newButton);
-    $("#button-display").append("<button class='btn btn-primary'>" + newButton + "</button>");
-});
+
 
 $(document).ready(function() {
-    for (var i = 0; i < gifs.length; i++) {
-        var button = gifs[i];
-        console.log(button);
-        $("#button-display").append("<button class='btn btn-primary'>" + button + "</button>");
-    };
-    
-    
-    
-        $("button").click(function() {
-            $("#images").empty();
-            var selected = $(this).text();
-            var queryURL =
-            "https://api.giphy.com/v1/gifs/search?q=" +
-            selected +
-            "&api_key=5Ctjw2fKwwsi9x1iIjvLF0sQ3SYD2jbY&limit=10";
-            console.log(selected);
-            
-            
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response) {
-                
-                result = response.data;
-               
-                for (var i = 0; i < result.length; i++){
-                    gif =  result[i];
-                    var imageDiv = $("<div>");
-                    
+  for (var i = 0; i < gifs.length; i++) {
+    var button = gifs[i];
+    console.log(button);
+    $("#button-display").append(
+      "<button class='btn btn-primary'>" + button + "</button>"
+      );
+      gifApp();
+    }
+  });
 
-                    var rating = gif.rating.toUpperCase();
-                    
-                    var p = $("<p>");
-                    p.text("Rating: (" + rating +")");
-                    
-                    
-                    var gifImage = $("<img>");
-                    
-                    gifImage.addClass("anImg");
-
-                    
-                    gifImage.attr("src", gif.images.fixed_height_still.url);
-                    
-                    gifImage.attr("data-still", gif.images.fixed_height_still.url);
-                    
-                    gifImage.attr("data-animate", gif.images.fixed_height.url);
-                    
-                    gifImage.attr("data-state", "still");
-                    
-                    
-                    
-                    imageDiv.append(gifImage);
-                    imageDiv.append(p);
-                    $("#images").append(imageDiv);
-                    
-                    
-                };
-                        $(".anImg").click(function(){
-                            
-                            var state = $(this).attr('data-state');
-                            
-                            if (state === 'still') {
-                                $(this).attr("src", $(this).data("animate"));
-                                $(this).attr('data-state', 'animate'); 
-                            } else {
-                                $(this).attr("src", $(this).data("still"));
-                                $(this).attr("data-state", "still");
-                            }
-                        });  
-                
-            });
-        });
-            
+    function gifApp(){
+    $("button").click(function() {
+      $("#images").empty();
+      var selected = $(this).text();
+      var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      selected +
+      "&api_key=5Ctjw2fKwwsi9x1iIjvLF0sQ3SYD2jbY&limit=10";
+      console.log(selected);
+      
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        result = response.data;
         
+        for (var i = 0; i < result.length; i++) {
+          gif = result[i];
+          var imageDiv = $("<div>");
+          
+          var rating = gif.rating.toUpperCase();
+          
+          var title = gif.title.toUpperCase();
+          console.log(title);
+          
+          var p = $("<p>");
+          p.text("Rating: (" + rating + ")");
+          
+          var h2 = $("<h2>")
+          
+          h2.text("Title: " + title);
+          
+          var hr = $("<hr>")
+          
+          var gifImage = $("<img>");
+          
+          gifImage.addClass("anImg");
+          
+          gifImage.attr("src", gif.images.fixed_height_still.url);
+          
+          gifImage.attr("data-still", gif.images.fixed_height_still.url);
+          
+          gifImage.attr("data-animate", gif.images.fixed_height.url);
+          
+          gifImage.attr("data-state", "still");
+          
+          imageDiv.append(h2);
+          imageDiv.append(gifImage);
+          imageDiv.append(p);
+          imageDiv.append(hr);
+          $("#images").append(imageDiv);
+        }
+        $(".anImg").click(function() {
+          var state = $(this).attr("data-state");
+          
+          if (state === "still") {
+            $(this).attr("src", $(this).data("animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).data("still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+      });
     });
-    
+  };
+  
+  
 
-
+  
+  $("#userSelected").click(function() {
+    event.preventDefault();
+    newButton = [];
+    var text = $("#select")
+      .val()
+      .trim();
+    newButton.push(text);
+    console.log(newButton);
+    $("#button-display").append(
+      "<button class='btn btn-primary'>" + newButton + "</button>"
+    );
+    gifApp();
+  });
